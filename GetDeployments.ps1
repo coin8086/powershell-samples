74092c413a49e4d5261b02767a22d9a5b9882b01
Robert Zhang <louirobert@gmail.com>
2023-12-26 15:02:37 +0800

    Add GetDeployments.ps1

---
+++ GetDeployments.ps1
@@ -0,0 +1,38 @@
+<#
+.SYNOPSIS
+Get deployments from a JSON file.
+
+.DESCRIPTION
+For Azure_Subscription_DP_Avoid_Plaintext_Secrets_Deployments
+#>
+
+param(
+    # Json file path
+    [Parameter(Mandatory)]
+    [string] $Path
+)
+
+$ErrorActionPreference = 'Stop'
+$InformationPreference = 'Continue'
+
+function GetDeploymentAndSecret {
+    param(
+        [string] $Source,
+        [string] $Secret
+    )
+    $pattern = '^/subscriptions/(?<sub>.+)/resourceGroups/(?<rg>.+)/providers/Microsoft.Resources/deployments/(?<deployment>.+)'
+    if (!($Source -match $pattern)) {
+        throw "'$Source' doesn't match '$pattern'!"
+    }
+    return [pscustomobject]@{
+        Sub = $Matches.sub
+        Rg = $Matches.rg
+        Deploymkent = $Matches.deployment
+        Secret = $Secret
+    }
+}
+
+$json = cat -Raw $Path | ConvertFrom-Json
+
+# NOTE: $json is a custom PSObject, not a Hashtable. So $json['xxx'] is not available.
+$json.'Truncated Data'.Detections | %{ GetDeploymentAndSecret $_.Source, $_.Secret }
